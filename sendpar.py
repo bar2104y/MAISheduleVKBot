78bc8b510bac438591bb50afcc4264269c8ecfb8
Yury Barichev <bar2104y@yandex.ru>
2019-09-15 00:08:56 +0300

    Комменты

--- sendpar.py
+++ sendpar.py
@@ -1,4 +1,4 @@
-import time,  sys
+import time, sys
 
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
@@ -9,28 +9,37 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from config import token, group_vk
 from ScheduleBot import ScheduleBot
 
+# Инициаизация класса бота
 Controller = ScheduleBot() 
 
-
+#Авторизация в ВК
 vk_session = vk_api.VkApi(token=token)
 vk = vk_session.get_api()
 
-number = sys.argv[1]
+number = sys.argv[1] # Номер пары читаем из терминала
 
-groups = Controller.get_groups()
+groups = Controller.get_groups() # Получаем все группы которые отслеживает бот
 
+#Перебираем все группы
 for group in groups:
+    # Сохраняем (кэшируем группу)
     Controller.save_data(group[0])
+    # Получаем всех пользователей из данной группы
     g_users = Controller.get_users_by_group(group[0])
     ids = ''
+    # Составляем список id, допусаем что в группе <= 100 человек
     for user in g_users:
         ids += user[0]+','
     
+    # Обрезаем последний(пустой)
     print(ids[:-1])
     
+    # Получаем текст уведомления
     mes = Controller.generate_text_lesson(group[0], number)
     
+    # Если сообщение есть
     if mes:
+        # Пробуем отправить сообщение
         try:
             vk.messages.send(
                     user_ids=ids,
